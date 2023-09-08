7a3853a122d3bd16a04d7c701bdcb4ed7ca5bb85
BrianTDevCode <briantriasm@gmail.com>
2023-09-08 19:06:34 -0300

    v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Routes, Route, HashRouter} from "react-router-dom";
+import {Routes, Route, HashRouter, BrowserRouter} from "react-router-dom";
 import {Home} from './components/Home/Home'
 import { Menu } from "./components/Menu/Menu";
 import { About } from "./components/About/About";
